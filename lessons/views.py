7460c1556ee27409a1dc08dd5d3781b2af39e52b
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-06 16:54:28 +0200

    Add buttons for getting category and place

--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Lesson
+from .models import Lesson, Category, Place
 # Create your views here.
 
 
@@ -11,8 +11,26 @@ def all_lessons(request):
 
     lessons = Lesson.objects.all()
 
+    categories = None  # if not set to None, error
+    places = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            lessons = lessons.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+    
+    if request.GET:
+        if 'place' in request.GET:
+            places = request.GET['place'].split(',')
+            lessons = lessons.filter(place__place__in=places)
+            places = Place.objects.filter(place__in=places)
+
+
     context = {
         'lessons': lessons,
+        'current_categories': categories,
+        'current_places': places,
     }
-    
+
     return render(request, 'lessons/lessons.html', context)
