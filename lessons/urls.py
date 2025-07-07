dd0548aa0bddef4a8e7af1c1bb5e0f48bd0d5047
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-07 12:23:42 +0200

    remove all things djangi-filter

--- lessons/urls.py
+++ lessons/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.all_lessons, name='lessons'),
-    path('filter/', views.filter_date, name="filter_date"),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Lesson, Category, Place
-from .filters import DateFilter
 # Create your views here.
 
 
@@ -27,8 +26,6 @@ def all_lessons(request):
             lessons = lessons.filter(place__place__in=places)
             places = Place.objects.filter(place__in=places)
 
-    
-
     context = {
         'lessons': lessons,
         'current_categories': categories,
@@ -37,15 +34,3 @@ def all_lessons(request):
 
     return render(request, 'lessons/lessons.html', context)
 
-
-def filter_date(request):
-    lessons = Lesson.objects.all()
-    date_filter = DateFilter(request.GET, queryset=lessons)
-    lessons = date_filter.qs
-
-    context = {
-        "filtered_lessons": lessons,
-        "date_filter": date_filter,
-        }
-
-    return render(request, "lessons/filter.html", context)
