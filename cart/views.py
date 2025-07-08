d0999a63b3ab072bb35af915c03a356c8c054320
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-08 22:12:24 +0200

    Add toast fucntionality

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from lessons.models import Lesson
 
 # Create your views here.
 
@@ -10,6 +12,9 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """Add lesson to cart"""
+    # get lessons for message
+    lesson = Lesson.objects.get(pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
 
     redirect_url = request.POST.get('redirect_url')
@@ -19,8 +24,10 @@ def add_to_cart(request, item_id):
     if item_id in list(cart.keys()):
         # should I prevent adding item to cart?
         cart[item_id] += quantity
+        messages.success(request, f'Added {lesson.name} to cart')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {lesson.name} to cart')
 
     request.session['cart'] = cart
 
