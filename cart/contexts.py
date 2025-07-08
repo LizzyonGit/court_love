8bb3be48dd5b281a0ef5e0e9cc71aa2100272e14
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-08 11:25:56 +0200

    Add cart contents and session available throught site

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from lessons.models import Lesson
 
 def cart_contents(request):
 
     cart_items = []
     product_count = 0
     grand_total = 0
+
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        lesson = get_object_or_404(Lesson, pk=item_id)
+        grand_total += quantity * lesson.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'lesson': lesson,
+        })
 
     context = {
         'cart_items': cart_items,
