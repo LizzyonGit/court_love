dde5ae5e154ffb41dcf898f94528c910ffa80767
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-10 16:18:53 +0200

    Add forms.py adapted from Boutique Ado

---
+++ checkout/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """Adapted from Boutique Ado"""
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full name',
+            'email': 'Email address',
+            'phone': 'Phone number',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
