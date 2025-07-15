006074496882e33baddca08decad7ffb0e52297d
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-15 15:22:15 +0200

    Start with Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
+from django.conf import settings
 from django.contrib import messages
+
 from .forms import OrderForm
+
+import stripe
+
+
+@require_POST
+def cache_checkout_data(request):
+    """before we call the confirm card payment method in the stripe javascript, we make a post request
+    to this view and give it the client secret from the payment intent, split it at the word secret to get the id
+    then add metadata to it to get the save info by the user
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            #'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        messages.error(request, 'Your payment could not be processed, try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     cart = request.session.get('cart', {})
--- court_love/settings.py
+++ court_love/settings.py
@@ -14,6 +14,9 @@ import os
 
 from pathlib import Path
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p-y3+os-7b&+r-ib#wd$zu^7$6rr-f978mnah+d+t1(^@v@ko6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -181,3 +184,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
