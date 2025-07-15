7a98ac7db0b74fdca341eebceb94edacc0be9124
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-15 18:29:48 +0200

    Continue Stripe

--- checkout/views.py
+++ checkout/views.py
@@ -4,41 +4,40 @@ from django.conf import settings
 from django.contrib import messages
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
 
 import stripe
 
-
-@require_POST
-def cache_checkout_data(request):
-    """before we call the confirm card payment method in the stripe javascript, we make a post request
-    to this view and give it the client secret from the payment intent, split it at the word secret to get the id
-    then add metadata to it to get the save info by the user
-    """
-    try:
-        pid = request.POST.get('client_secret').split('_secret')[0]
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        stripe.PaymentIntent.modify(pid, metadata={
-            'cart': json.dumps(request.session.get('cart', {})),
-            #'save_info': request.POST.get('save_info'),
-            'username': request.user,
-        })
-        return HttpResponse(status=200)
-    
-    except Exception as e:
-        messages.error(request, 'Your payment could not be processed, try again later.')
-        return HttpResponse(content=e, status=400)
-
-
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is empty")
         return redirect(reverse('lessons'))
+ 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
