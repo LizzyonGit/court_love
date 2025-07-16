e2a396932cd44244cee49b1d55a229ab794f0fa4
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-16 11:17:38 +0200

    Fix debug error, add js for card element

---
+++ checkout/static/checkout/js/stripe_elements.js
@@ -0,0 +1,25 @@
+/*Adapted from Boutique Ado*/
+
+let stripe_public_key = document.querySelector('#id_stripe_public_key').textContent.slice(1, -1);
+let client_secret = document.querySelector('#id_client_secret').textContent.slice(1, -1);
+console.log(stripe_public_key)
+console.log(client_secret)
+const stripe = Stripe(stripe_public_key);
+let elements = stripe.elements();
+let style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+const card = elements.create('card', {style: style});
+card.mount('#card-element');
