c68e812242d17cef91f4251d63bd484e648a290f
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-16 11:54:10 +0200

    Adjust styling checkout form

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,15 +1,14 @@
-/*Adapted from Boutique Ado*/
+/*Adapted from Boutique Ado, js instead of jQuery*/
 
 let stripe_public_key = document.querySelector('#id_stripe_public_key').textContent.slice(1, -1);
 let client_secret = document.querySelector('#id_client_secret').textContent.slice(1, -1);
-console.log(stripe_public_key)
-console.log(client_secret)
+
 const stripe = Stripe(stripe_public_key);
 let elements = stripe.elements();
 let style = {
     base: {
         color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontFamily: '"IBM Plex Serif", sans-serif',
         fontSmoothing: 'antialiased',
         fontSize: '16px',
         '::placeholder': {
