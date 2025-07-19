64dde004497b69d3b5adf963d17011a3a4aab05a
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-07-19 15:46:56 +0200

    Add checkout_success functionality

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from .forms import OrderForm
 from cart.contexts import cart_contents
 
+from lessons.models import Lesson
+from .models import OrderLineItem, Order
 
 import stripe
 
@@ -13,31 +15,88 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(request, "Your cart is empty")
-        return redirect(reverse('lessons'))
- 
-    current_cart = cart_contents(request)
-    total = current_cart['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
-    order_form = OrderForm()
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone': request.POST['phone'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, quantity in cart.items():
+                try:
+                    lesson = Lesson.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        lesson=lesson,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
+                    
+                except Lesson.DoesNotExist:
+                    messages.error(request, (
+                        "One of the lesson in your cart was not found in our database. "
+                        "Call us for assistance.")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_cart'))
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Double check your information.')
 
-    template = 'checkout/checkout.html'
+    else:
+
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, "Your cart is empty")
+            return redirect(reverse('lessons'))
+    
+        current_cart = cart_contents(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
+
+        if not stripe_public_key:
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
+
+        template = 'checkout/checkout.html'
+        context = {
+            'order_form': order_form,
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
+        }
+
+        return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed. \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+    
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'checkout/checkout_success.html'
     context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'order': order,
     }
-
     return render(request, template, context)
