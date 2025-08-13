781d58c3866c9fd4bbfd5923bff8096b2f2f4873
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-08-13 14:07:46 +0200

    adjust image slection profile

--- profiles/widgets.py
+++ profiles/widgets.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # from Boutiq Ado
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove')
+    clear_checkbox_label = _('Remove my profile image')
+    initial_text = _('Select an image and click Update information below')
     input_text = _('')
     template_name = 'profiles/custom_widget_templates/custom_clearable_file_input.html'
