906ff2f2459be08b94e39d9cf5f36cf6b673436d
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-08-13 15:23:10 +0200

    redo image fields, now remove is visible when no image, need to fix

--- profiles/widgets.py
+++ profiles/widgets.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # from Boutiq Ado
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove my profile image')
+    clear_checkbox_label = _('Remove current profile image after Update')
     initial_text = _('Select an image and click Update below')
     input_text = _('')
     template_name = 'profiles/custom_widget_templates/custom_clearable_file_input.html'
