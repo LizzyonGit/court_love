83338a1883eb4c4b242e63fedf08802b85664d68
LizzyonGit <lizzyvanrhijn@hotmail.com>
2025-08-22 15:30:14 +0200

    shorten text after issue small screens

--- profiles/widgets.py
+++ profiles/widgets.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 # from Boutiqe Ado
 class CustomClearableFileInput(ClearableFileInput):
-    clear_checkbox_label = _('Remove current profile image after Update')
+    clear_checkbox_label = _('Remove current image after Update')
     initial_text = _('Select an image and click Update below')
     input_text = _('')
     template_name = (
